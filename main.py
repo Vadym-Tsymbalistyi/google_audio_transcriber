a166113f43636c91d6e1d6c88f41736f65669f24
vadim <vadumcimbalist@gmail.com>
2025-10-05 00:33:21 +0300

    ref

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from drive_client import GoogleDriveClient
-
-
-def main():
-    drive_client = GoogleDriveClient()
-    copied_files = drive_client.copy_to_workspace_folder()
-    print("\nList of copied files in the work folder Drive:")
-    for f in copied_files:
-        print(f"{f['name']} (id={f['id']})")
-
+from controller import MainController
 
 if __name__ == "__main__":
-    main()
+    controller = MainController()
+    controller.process_files()
