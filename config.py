c0a3103a4fef8ccdee287724bd58918a5eaf7a4d
vadim <vadumcimbalist@gmail.com>
2025-10-05 00:32:28 +0300

    Add nev Config

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ class Config:
     WORKSPACE_FOLDER_ID = os.getenv('WORKSPACE_FOLDER_ID')
     DRIVE_FOLDER_ID = os.getenv('DRIVE_FOLDER_ID')
     SHEET_TEMPLATE_ID=os.getenv('SHEET_TEMPLATE_ID')
-    SCOPES = ['https://www.googleapis.com/auth/drive']
+    SCOPES = os.getenv('SCOPES')
+    WORKSPACE = os.path.join(os.path.dirname(__file__), "workspace")
+    os.makedirs(WORKSPACE, exist_ok=True)
