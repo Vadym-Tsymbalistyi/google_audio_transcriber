3e5b27338afee86a4a9b9949cb2ce1393223e6ea
vadim <vadumcimbalist@gmail.com>
2025-10-06 16:01:43 +0300

    Added Transcriber class using Whisper model to transcribe audio

--- transcriber.py
+++ transcriber.py
@@ -1,10 +1,19 @@
 import whisper
-import  os
+import os
+
 class Transcriber:
-    def __init__(self, model_name="base"):
+    def __init__(self, model_name="medium"):
         self.model = whisper.load_model(model_name)
 
     def transcribe(self, audio_path):
-        print(f"We transcribe the file{os.path.basename(audio_path)} ...")
-        result = self.model.transcribe(audio_path)
-        return result["text"]
+        file_name = os.path.basename(audio_path)
+        print(f" I transcribe the file: {file_name} ...")
+
+        result = self.model.transcribe(
+            audio_path,
+            task="transcribe",
+            language="uk",
+            temperature=0.0
+        )
+        text = result["text"].strip()
+        return text
