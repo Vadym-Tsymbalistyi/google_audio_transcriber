4e4437d15fbb5e72e4d554180e558b6b5f02f327
vadim <vadumcimbalist@gmail.com>
2025-10-06 16:37:16 +0300

    ref config

--- config.py
+++ config.py
@@ -6,7 +6,13 @@ class Config:
     WORKSPACE_FOLDER_ID = os.getenv('WORKSPACE_FOLDER_ID')
     DRIVE_FOLDER_ID = os.getenv('DRIVE_FOLDER_ID')
     SHEET_TEMPLATE_ID=os.getenv('SHEET_TEMPLATE_ID')
-    SCOPES = ['https://www.googleapis.com/auth/drive']
-
+    SCOPES = os.getenv('SCOPES')
+    CREDENTIALS_PATH=os.getenv('CREDENTIALS_PATH')
+    CREDENTIALS=os.getenv('CREDENTIALS')
+    GOOGLE_SHEET_ID = os.getenv('GOOGLE_SHEET_ID')
     WORKSPACE = os.path.join(os.path.dirname(__file__), "workspace")
     os.makedirs(WORKSPACE, exist_ok=True)
+
+    AZURE_OPENAI_ENDPOINT=os.getenv('AZURE_OPENAI_ENDPOINT')
+    AZURE_OPENAI_KEY=os.getenv('AZURE_OPENAI_KEY')
+    CHAT_COMPLETION_NAME=os.getenv('CHAT_COMPLETION_NAME')
